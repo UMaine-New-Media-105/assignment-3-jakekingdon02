bb776d94612fe74e2fabebe29e5edc35b5c0add3
jakekingdon02 <124187370+jakekingdon02@users.noreply.github.com>
2023-02-28 21:58:01 -0500

    Assignment3Chal1.js
    I created a game where the baseball falls from the top of the canvas every time "mouseIsPressed". When the baseball is caught by the glove, it keeps the ball in place until you restart the "game". 
    
    Link: https://editor.p5js.org/Jake_Kingdon02/sketches/ai23FOEDY

---
+++ Assignment3Chal1.js
@@ -0,0 +1,86 @@
+// Jake Kingdon
+// Assignment 3 Question 1
+// Making my emoji sprite be caught by the catcher's glove.
+
+let ballX, ballY; // x and y coordinates of the ball
+let ballSize = 30; // diameter of the ball
+let ballSpeedX, ballSpeedY; // speed of the ball
+let gloveX, gloveY; // x and y coordinates of the glove
+let gloveWidth = 100; // width of the glove
+let gloveHeight = 50; // height of the glove
+let caught = false; // whether the ball has been caught or not
+
+function setup() {
+  createCanvas(400, 400);
+  resetBall();
+  gloveX = width / 2 - gloveWidth / 2; // place the glove in the middle of the canvas
+  gloveY = height - gloveHeight - 10; // place the glove near the bottom of the canvas
+}
+
+function draw() {
+  background(220);
+  moveBall();
+  checkCollision();
+  drawBall();
+  drawGlove();
+}
+
+// Ball size
+function resetBall() {
+  ballX = random(ballSize, width - ballSize);
+  ballY = random(ballSize, height / 2);
+  ballSpeedX = random(-5, 5);
+  ballSpeedY = random(3, 7);
+  caught = false;
+}
+
+// Speed of the ball
+function moveBall() {
+  if (!caught) {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+    if (ballX + ballSize / 2 > width || ballX - ballSize / 2 < 0) {
+      ballSpeedX *= -1;
+    }
+    if (ballY + ballSize / 2 > height || ballY - ballSize / 2 < 0) {
+      ballSpeedY *= -1;
+    }
+  }
+}
+
+// Initial collision
+function checkCollision() {
+  if (
+    !caught &&
+    ballY + ballSize / 2 > gloveY &&
+    ballX > gloveX &&
+    ballX < gloveX + gloveWidth
+  ) {
+    ballSpeedY *= -1;
+    caught = true;
+  }
+}
+
+// Draw the baseball
+function drawBall() {
+  fill("white");
+  ellipse(ballX, ballY, ballSize);
+}
+
+// Draw the glove
+function drawGlove() {
+  fill("brown");
+  rect(gloveX, gloveY, gloveWidth, gloveHeight);
+}
+
+function mouseMoved() {
+  if (!caught) {
+    gloveX = mouseX - gloveWidth / 2;
+  }
+}
+
+function mousePressed() {
+  if (caught) {
+    resetBall();
+  }
+}
